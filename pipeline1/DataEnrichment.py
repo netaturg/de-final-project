8b8078d9f5689a66368a4fc854f8cce9ca5586b5
netaturg <netatur14@gmail.com>
2022-04-16 23:09:59 +0300

    .

--- pipeline1/DataEnrichment.py
+++ pipeline1/DataEnrichment.py
@@ -9,7 +9,29 @@ import json
 from datetime import date
 
 client = boto3.client('kinesis')
+
+#country code.
+country_code_url = "jdbc:mysql://database-1.cxxzuzxqj9zm.us-east-1.rds.amazonaws.com:3306/jobs_project"
+dbtable = "CountryCode"
+user = "root"
+password = "root1234"
+
+#stream
+stream_format = 'kinesis'
 stream_name='clean-data'
+endpointUrl = 'https://kinesis.us-east-1.amazonaws.com'
+region = 'us-east-1'
+awsAccessKeyId = ''
+awsSecretKey = ''
+startingposition = 'TRIM_HORIZON'
+
+#s3
+current_date = date.today()
+mode = "append"
+s3_format = "json"
+processingTime = '30 seconds'
+s3_path = "s3a://stack-overflow-neta/jobs/{}"
+s3_checkpointLocation = "s3a://stack-overflow-neta/checkpoint"
 
 spark = SparkSession.builder \
          .master('local[*]') \
@@ -17,21 +39,21 @@ spark = SparkSession.builder \
          .getOrCreate()
 
 df_countries = spark.read.format("jdbc")\
-    .option("url", "jdbc:mysql://database-1.cxxzuzxqj9zm.us-east-1.rds.amazonaws.com:3306/jobs_project") \
-    .option("driver", "com.mysql.jdbc.Driver").option("dbtable", "CountryCode") \
-    .option("user", "root").option("password", "root1234").load()
+    .option("url", country_code_url) \
+    .option("driver", "com.mysql.jdbc.Driver").option("dbtable", dbtable) \
+    .option("user", user).option("password", password).load()
 
 df_countries.printSchema()
 
 kinesis = spark \
         .readStream \
-        .format('kinesis') \
+        .format(stream_format) \
         .option('streamName', stream_name) \
-        .option('endpointUrl', 'https://kinesis.us-east-1.amazonaws.com')\
-        .option('region', 'us-east-1') \
-        .option('awsAccessKeyId', '') \
-        .option('awsSecretKey', '') \
-        .option('startingposition', 'TRIM_HORIZON')\
+        .option('endpointUrl', endpointUrl)\
+        .option('region', region) \
+        .option('awsAccessKeyId', awsAccessKeyId) \
+        .option('awsSecretKey', awsSecretKey) \
+        .option('startingposition', startingposition)\
         .load()
 
 schema = StructType([
@@ -56,16 +78,14 @@ df = df.join(df_countries ,df.country == df_countries.Code ,"left")\
            .drop('country') \
            .toDF("title","date_posted","description","detail_url","state","city","company_name","Employment type", "Name", "Code")
 
-current_date = date.today()
-
 df.writeStream \
-        .outputMode("append") \
-        .format("json") \
-        .trigger(processingTime='30 seconds') \
+        .outputMode(mode) \
+        .format(s3_format) \
+        .trigger(processingTime=processingTime) \
         .repartition(1)\
-        .option("path", "s3a://stack-overflow-neta/jobs/{}".format(current_date)) \
+        .option("path", s3_path.format(current_date)) \
         .option("header", True) \
-        .option("checkpointLocation", "s3a://stack-overflow-neta/checkpoint") \
+        .option("checkpointLocation", s3_checkpointLocation) \
         .start() \
         .awaitTermination()
 
